aa4e48e66641fe4b8caf5330333501e2040fa327
paran0idl <lizu0817@me.com>
2016-11-04 15:24:12 +0800

    Initial Commit

---
+++ tickets/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tickets
+//
+//  Created by 李祖翔 on 2016/11/4.
+//  Copyright © 2016年 李祖翔. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tickets/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tickets
+//
+//  Created by 李祖翔 on 2016/11/4.
+//  Copyright © 2016年 李祖翔. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
